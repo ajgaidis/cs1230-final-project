4f02205d016952f4b214fa7a92f034facab7843c
Neophytos Christou <neophytos_christou@brown.edu>
2022-12-07 13:54:59 -0500

    Some stuff

--- wall-hack/main.c
+++ wall-hack/main.c
@@ -1,8 +1,12 @@
 #include <errno.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/uio.h>
 
 #define PROG_NAME "csgo_linux64"
 
@@ -13,14 +17,75 @@
 
 #define PATH_LEN  64
 
+/* For wireframe WH */
 #define INSN_OFFSET         0x7f1463
 #define INSN_OPERAND_OFFSET 0x2
+
+/* For glow WH */
+#define GLOWOBJMGR_OFFSET 0x2b9cf80
+#define GLOWOBJDEF_SIZE   64
 
 #define handle_error_en(en, msg) \
   do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 #define handle_error(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+struct GlowObjectDefinition_t
+{
+    int m_nNextFreeSlot;
+    uintptr_t m_pEntity;
+    float m_vGlowColorX;
+    float m_vGlowColorY;
+    float m_vGlowColorZ;
+    float m_vGlowAlpha;
+    unsigned char m_bGlowAlphaCappedByRenderAlpha;
+    unsigned char padding[3];
+    float m_flGlowAlphaFunctionOfMaxVelocity;
+    float m_flGlowAlphaMax;
+    float m_flGlowPulseOverdrive;
+    unsigned char m_renderWhenOccluded;
+    unsigned char m_renderWhenUnoccluded;
+    int m_nRenderStyle;
+    int m_nSplitScreenSlot;
+};
+
+int read_from_proc(pid_t pid, uintptr_t addr_to_read, void *buffer, size_t len)
+{
+    struct iovec local[1] = {};
+    struct iovec remote[1] = {};
+    int errsv = 0;
+    ssize_t nread = 0;
+
+    local[0].iov_len = len;
+    local[0].iov_base = buffer;
+
+    remote[0].iov_base = (void *) addr_to_read;
+    remote[0].iov_len = local[0].iov_len;
+
+    nread = process_vm_readv(pid, local, 2, remote, 1, 0);
+
+    if (nread != local[0].iov_len)
+    {
+        handle_error("process_vm_readv");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void print_glow_obj_def(struct GlowObjectDefinition_t *obj)
+{
+    printf("m_nNextFreeSlot: %d\n", obj->m_nNextFreeSlot);
+    printf("m_pEntity: %#lx\n", obj->m_pEntity);
+    printf("m_vGlowColorX: %.3f\n", obj->m_vGlowColorX);
+    printf("m_vGlowColorY: %.3f\n", obj->m_vGlowColorY);
+    printf("m_vGlowColorZ: %.3f\n", obj->m_vGlowColorZ);
+    printf("m_vGlowAlpha: %.3f\n", obj->m_vGlowAlpha);
+    printf("m_vGlowAlphaMax: %.3f\n", obj->m_flGlowAlphaMax);
+    printf("m_renderWhenOccluded: %d\n", obj->m_renderWhenOccluded);
+    printf("m_renderWhenUnccluded: %d\n", obj->m_renderWhenUnoccluded);
+}
 
 
 pid_t get_pid(void)
@@ -41,7 +106,7 @@ pid_t get_pid(void)
     handle_error("strtoul");
 
   /* cleanup */
-  if (pclose(cmd_stream) == NULL)
+  if (pclose(cmd_stream) == -1)
     handle_error("pclose");
 
   return pid;
@@ -65,11 +130,32 @@ uintptr_t get_base_addr(void)
     handle_error("strtoul");
 
   /* cleanup */
-  if (pclose(cmd_stream) == NULL)
+  if (pclose(cmd_stream) == -1)
     handle_error("pclose");
 
   return base_addr;
 }
+
+void print_hex_buf(unsigned char *buf, int sz)
+{
+    int i;
+    for (i = 0; i < sz; i++)
+    {
+        if (i > 0) printf(":");
+        printf("%02x", buf[i]);
+    }
+    printf("\n");
+}
+
+unsigned long unpack(unsigned char *buf)
+{
+    unsigned long res = 0;
+	for (int i = 0; i < 8; i++) {
+        res += buf[i] << (i * 8);
+	}
+	return res;
+}
+
 
 int main(void)
 {
@@ -79,9 +165,10 @@ int main(void)
 
   /* get process pid */
   pid = get_pid();
+  printf("Got %s pid: %d\n", PROG_NAME, pid);
 
   /* change working directory to make accessing files easy */
-  if (snprintf(path, PATH_LEN, "/proc/%.10s", pid) < 0)
+  if (snprintf(path, PATH_LEN, "/proc/%d", pid) < 0)
     handle_error_en(EINVAL, "snprintf");
 
   if (chdir(path) == -1)
@@ -89,9 +176,25 @@ int main(void)
 
   /* get base address of loaded library */
   base_addr = get_base_addr();
+  printf("Library client_client.so located at %#lx\n", base_addr);
 
-  /* get instruction address to patch */
-  offset_addr = base_addr + INSN_OFFSET + INSN_OPERAND_OFFSET;
+  // Deref this to get the list
+  uintptr_t s_GlowObjectManager = base_addr + GLOWOBJMGR_OFFSET;
+  printf("s_GlowObjectManager located at %#lx\n", s_GlowObjectManager);
+  unsigned char buf[9];
+  memset(&buf, 0, 9);
+  read_from_proc(pid, s_GlowObjectManager, buf, 8);
+  print_hex_buf(buf, 9);
+  uintptr_t m_GlowObjectDefinitions = unpack(buf);
+  printf("GlowObjectDefinisions list located at %#lx\n", m_GlowObjectDefinitions);
+
+  struct GlowObjectDefinition_t glow_obj_def;
+  memset((void *)&glow_obj_def, 0, GLOWOBJDEF_SIZE);
+  read_from_proc(pid, m_GlowObjectDefinitions, &glow_obj_def, GLOWOBJDEF_SIZE);
+  print_hex_buf((unsigned char *)&glow_obj_def, GLOWOBJDEF_SIZE);
+  print_glow_obj_def(&glow_obj_def);
+  /* /1* get instruction address to patch *1/ */
+  /* offset_addr = base_addr + INSN_OFFSET + INSN_OPERAND_OFFSET; */
 
   /* overwrite the byte to enable wall hacks */
 }
